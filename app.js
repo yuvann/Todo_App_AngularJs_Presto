389aefc34346c9c7ad4ba4131b23737d5490f6e2
yuvann <yuvan2244@gmail.com>
2017-12-09 16:28:50 +0530

    updated

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
+
 handleScreenAction = (state) => {
   switch (state.tag) {
     case "InitScreenInit":break;
     case "InitScreenAddTodo":
-      application.count = JSON.parse(localStorage.getItem("todos")).length
       update(state.contents);
       break;
     case "InitScreenRemoveTodo":
-      application.count = JSON.parse(localStorage.getItem("todos")).length
       update(state.contents);
       break;
     case "InitScreenUpdateTodo":
-      application.count = JSON.parse(localStorage.getItem("todos")).length
       update(state.contents);
       break;
     default: console.log("Invalid Tag Passed", state.tag);
@@ -18,15 +16,18 @@ handleScreenAction = (state) => {
   }
 }
 
+update = (val) => {
+  if(val==="FAILURE") {
+    alert("Server Doesn't respond ; Try to start server using 'npm start' ;")
+  }
+ var app = angular.element(document.getElementById('app')).scope()
+ app.todos = JSON.parse(localStorage.getItem('todos') || "[]");
+ app.count = JSON.parse(localStorage.getItem('todos') || "[]").length;
+ app.$applyAsync()
+
+}
 
 window.showScreen = function (callBack, data) {
     window.callBack = callBack;
     handleScreenAction(data);
 };
-
-update = (val) => {
-   if(val==="FAILURE") {
-    alert("Server Doesn't respond ; Try to start server using 'npm start' ;")
-  }
-    application.todos = JSON.parse(localStorage.getItem("todos"))
-}
